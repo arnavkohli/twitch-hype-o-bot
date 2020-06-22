7c80487dcbcc4703ddf79f4100237a815b8dc849
Arnav Kohli <arnavkohli@gmail.com>
2020-06-23 02:29:25 +0530

    Update bluetooth.py

--- bluetooth.py
+++ bluetooth.py
@@ -11,16 +11,3 @@ class BluetoothSerialPortController:
 	@classmethod
 	def slow(cls):
 		cls.ser.write(b'\x00')
-
-# class bcolors:
-#     HEADER = '\033[95m'
-#     OKBLUE = '\033[94m'
-#     OKGREEN = '\033[92m'
-#     WARNING = '\033[93m'
-#     FAIL = '\033[91m'
-#     ENDC = '\033[0m'
-#     BOLD = '\033[1m'
-#     UNDERLINE = '\033[4m'
-
-# if __name__ == '__main__':
-# 	print (bcolors.HEADER + 'LOL')
